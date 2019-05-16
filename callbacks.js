022138fd5b0250b9e8ecfab36bd601819d4cf9b2
Vlado <44269923+post-punk@users.noreply.github.com>
2019-05-16 16:17:21 +0200

    Add files via upload

---
+++ callbacks.js
@@ -0,0 +1,30 @@
+const posts = [
+    {   
+        title: 'post one',
+        body: 'this is post one'
+    },
+    {
+        title: 'post two',
+        body: 'this is post two'
+    }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post, kolbek) {
+    setTimeout(() => {
+        posts.push(post);
+        kolbek();
+    }, 2000);
+}
+
+
+createPost({ title: 'post three', body: 'this is post three'}, getPosts);
